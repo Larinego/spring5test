d9621f70a6af11f15e0a64bba5845065fc8fcda1
alexanderla <larinego.com>
2019-05-03 19:09:06 +0300

    Commit

---
+++ src/main/java/com/larinego/spring5test/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package com.larinego.spring5test.controllers;
+
+import com.larinego.spring5test.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+
+        return "authors";
+    }
+}
---
+++ src/main/java/com/larinego/spring5test/controllers/BookController.java
@@ -0,0 +1,24 @@
+package com.larinego.spring5test.controllers;
+
+import com.larinego.spring5test.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks (Model model){
+        model.addAttribute("books",bookRepository.findAll());
+
+        return "books";
+    }
+
+}
